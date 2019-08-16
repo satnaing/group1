012805a5c8b34b20f7064e36189733430a11920f
satnaing <satnaingeklage@gmail.com>
2019-08-16 08:39:35 +0630

    World Class is updated!

--- src/main/java/com/napier/group1/World.java
+++ src/main/java/com/napier/group1/World.java
@@ -1,5 +1,35 @@
 package com.napier.group1;
 
+import java.sql.*;
+
+/**
+ * Represents a world class
+ */
 public class World {
 
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Country Code
+     */
+    public String Code;
+
+    /**
+     * Country Name
+     */
+    public String Name;
+
+    /**
+     * Country Population
+     */
+    public int Population;
+
+
+
+
 }
+
+
