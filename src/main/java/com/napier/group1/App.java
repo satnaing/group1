b5f5bbfc17508e210fc8c967cb99391747f13b17
satnaing <satnaingeklage@gmail.com>
2019-08-16 10:05:13 +0630

    Top populated countries in region and continent can be seen now.

--- src/main/java/com/napier/group1/App.java
+++ src/main/java/com/napier/group1/App.java
@@ -1,25 +1,37 @@
 package com.napier.group1;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class App
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
 
-        // Get Employee
-        World wd = a.getCountry();
-        // Display results
-//        a.displayCountry(wd);
+        // Display Top Populated countries in the world
+        a.getCountry();
+
+        // Display Top Populated countries in a continent
+        a.getCountryInContinent();
+
+        // Display Top Populated countries in a continent
+        a.getCountryInContinent();
+
+        // Display Top Populated countries in a region
+        a.getCountryInRegion();
 
         // Disconnect from database
         a.disconnect();
     }
+
+
     /**
      * Connection to MySQL database.
      */
@@ -98,32 +110,103 @@ public class App
                             + "ORDER BY Population DESC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
             // Check one is returned
-//            if (rset.next())
-//            {
-                while (rset.next()){
-                    World wd = new World();
-                    wd.Name = rset.getString("Name");
-                    wd.Population = rset.getInt("Population");
-                    System.out.println(
-                        "Country:" + wd.Name + "\n" +
-                            "Population: " + wd.Population + "\n");
-                }
-                return null;
-//                return wd;
-//            }
-//            else
-//                return null;
+
+            while (rset.next()){
+                World wd = new World();
+                wd.Name = rset.getString("Name");
+                wd.Population = rset.getInt("Population");
+                System.out.println(
+                    "Country:" + wd.Name + "\n" +
+                        "Population: " + wd.Population + "\n");
+            }
+            return null;
         }
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
+            System.out.println("Failed to get country details");
             return null;
         }
     }
 
+    public World getCountryInContinent()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create Asia string for SQL statement
+            String strCont =
+                    "SELECT Name, Population "
+                            + "FROM country WHERE continent = 'Asia' "
+                            + "ORDER BY Population DESC";
+
+            // Execute SQL statement
+            ResultSet rsetStr = stmt.executeQuery(strCont);
+            // Check one is returned
+
+            // Displaying Countries of Asia Continent
+            System.out.println("Continent : Asia");
+            System.out.println("-----------------");
+            while (rsetStr.next()){
+                World wd = new World();
+                wd.Name = rsetStr.getString("Name");
+                wd.Population = rsetStr.getInt("Population");
+                System.out.println(
+                        "Country:" + wd.Name + "\n" +
+                                "Population: " + wd.Population + "\n");
+            }
+            System.out.println("-----------------"+ "\n");
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details" + e);
+            return null;
+        }
+    }
+
+    public World getCountryInRegion()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create Asia string for SQL statement
+            String strCont =
+                    "SELECT Name, Population "
+                            + "FROM country WHERE Region = 'Southeast Asia' "
+                            + "ORDER BY Population DESC";
+
+            // Execute SQL statement
+            ResultSet rsetStr = stmt.executeQuery(strCont);
+            // Check one is returned
+
+            // Displaying Countries of Asia Continent
+            System.out.println("Region : Southeast Asia");
+            System.out.println("-----------------");
+            while (rsetStr.next()){
+                World wd = new World();
+                wd.Name = rsetStr.getString("Name");
+                wd.Population = rsetStr.getInt("Population");
+                System.out.println(
+                        "Country:" + wd.Name + "\n" +
+                                "Population: " + wd.Population + "\n");
+            }
+            System.out.println("-----------------"+ "\n");
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details" + e);
+            return null;
+        }
+    }
 //    public void displayCountry(World wd)
 //    {
 //        if (wd != null)
