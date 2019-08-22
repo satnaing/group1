91bfa602d60f7c7a1f18502aed5d6cc2a0ead2f1
thantzintzw <53735656+thantzintzw@users.noreply.github.com>
2019-08-22 15:23:56 +0630

    add appIntegrationTest.java in test Folder

---
+++ src/Test/com/napier/group1/AppIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.napier.group1;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    @Test
+    void testgetCountry()
+    {
+        world emp = app.getCountry();
+        assertEquals(emp.Name, "Myanmar");
+        assertEquals(emp.Code, "KOR");
+        assertEquals(emp.Population, 229060);
+
+
+    }
+}
